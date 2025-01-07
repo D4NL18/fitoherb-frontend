dfbb5213e6ae1387f8057a3d44aefa4b3f08acef
Daniel Marinho <danielmarinho8@hotmail.com>
2025-01-07 14:42:40 -0300

    footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { ProductsComponent } from './views/products/products.component';
+import { SuppliersComponent } from './views/suppliers/suppliers.component';
+import { ContactComponent } from './views/contact/contact.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: HomeComponent
+    },
+    {
+        path: "about",
+        component: AboutComponent
+    },
+    {
+        path: "products",
+        component: ProductsComponent
+    },
+    {
+        path: "suppliers",
+        component: SuppliersComponent
+    },
+    {
+        path: "contact",
+        component: ContactComponent
     }
 ];
