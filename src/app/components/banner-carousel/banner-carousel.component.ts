3653d3f20bc3ba09eebfa398fa86cc068462e234
Daniel Marinho <danielmarinho8@hotmail.com>
2025-01-07 15:08:10 -0300

    feat: banner carousel

--- src/app/components/banner-carousel/banner-carousel.component.ts
+++ src/app/components/banner-carousel/banner-carousel.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-banner-carousel',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule],
   templateUrl: './banner-carousel.component.html',
   styleUrl: './banner-carousel.component.scss'
 })
 export class BannerCarouselComponent {
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { BannerCarouselComponent } from '../../components/banner-carousel/banner-carousel.component';
+import { ImagesCarouselComponent } from "../../components/images-carousel/images-carousel.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, BannerCarouselComponent, ImagesCarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
