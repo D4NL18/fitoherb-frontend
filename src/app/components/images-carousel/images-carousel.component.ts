e4ad853a43f1a4aef5ea1d7904273a6281f77cbc
Daniel Marinho <danielmarinho8@hotmail.com>
2025-01-07 14:06:01 -0300

    navbar

---
+++ src/app/components/images-carousel/images-carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-images-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './images-carousel.component.html',
+  styleUrl: './images-carousel.component.scss'
+})
+export class ImagesCarouselComponent {
+
+}
