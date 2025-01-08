2e416eff52923333bb98589c3c4b279397842410
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2025-01-08 15:59:19 -0300

    feat: suppliers page

--- src/app/views/products/products.component.ts
+++ src/app/views/products/products.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../components/footer/footer.component";
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, NavbarComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
--- src/app/views/suppliers/suppliers.component.ts
+++ src/app/views/suppliers/suppliers.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { FooterComponent } from "../../components/footer/footer.component";
+import { ImagesCarouselComponent } from "../../components/images-carousel/images-carousel.component";
 
 @Component({
   selector: 'app-suppliers',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent, ImagesCarouselComponent],
   templateUrl: './suppliers.component.html',
   styleUrl: './suppliers.component.scss'
 })
