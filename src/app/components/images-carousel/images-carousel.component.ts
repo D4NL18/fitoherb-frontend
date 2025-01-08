c2faa6df5950e8375baa1c82f8c7092e98e81412
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2025-01-08 14:59:48 -0300

    fix: image carousel can have any img size or number of images

--- src/app/components/images-carousel/images-carousel.component.ts
+++ src/app/components/images-carousel/images-carousel.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-images-carousel',
@@ -9,11 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './images-carousel.component.scss'
 })
 export class ImagesCarouselComponent {
+
+  @Input() numImg: number = 3;
+  @Input() sizeImg: number = 300 
+
   images = [
     'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/640px-Grosser_Panda.JPG',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4fb9-Mrcz1_0IhrLpCFzG7zisGz8OvqzNog&s',
+    'https://static.mundoeducacao.uol.com.br/mundoeducacao/2023/03/panda-vermelho-arvore.jpg',
     'https://s2-oglobo.glbimg.com/4_XjWnMCDLAxztquqgDyzw9TPv8=/0x0:1400x934/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2023/7/I/EMZ0YwQGG6bGeZwCMRwg/pandas.jpg',
+    'https://static.preparaenem.com/2023/03/panda-vermelho-deitado-no-galho-de-uma-arvore.jpg',
     'https://s4.static.brasilescola.uol.com.br/img/2019/09/panda.jpg',
+    'https://ambientalistasemrede.wordpress.com/wp-content/uploads/2012/06/282703_304853429603076_349697252_n.jpg?w=960',
     'https://conteudo.imguol.com.br/c/noticias/bc/2020/08/18/panda-gigante-mei-xiang-no-zoologico-nacional-de-washington-nos-eua-1597792594233_v2_900x506.jpg',
   ];
 
@@ -24,15 +30,15 @@ export class ImagesCarouselComponent {
 
   next() {
     this.currentIndex = (this.currentIndex + 1);
-    if (this.currentIndex === this.images.length - 2) {
+    if (this.currentIndex === this.images.length - (this.numImg - 1)) {
       this.currentIndex = 0
     }
     this.updateTransform();
   }
 
   prev() {
-    if(this.currentIndex === 0) {
-      this.currentIndex = 2
+    if(this.currentIndex === 0 && this.numImg != 1) {
+      this.currentIndex = (this.numImg - 1)
     }else {
       this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
     }
@@ -41,7 +47,7 @@ export class ImagesCarouselComponent {
   }
 
   updateTransform() {
-    const offset = this.currentIndex * -330;
+    const offset = this.currentIndex * - (this.sizeImg);
     this.transform = `translateX(${offset}px)`;
   }
 }
