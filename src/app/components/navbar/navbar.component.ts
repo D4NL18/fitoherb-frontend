6de0467b16bc8db7a78aa76fecdef61b92b47e21
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2025-01-08 14:40:48 -0300

    fix: navbar fixed and hover property

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+constructor(private router: Router) {}
 
+thisPage: 'home' | 'about' | 'products' | 'suppliers' | 'contact' = 'home'
+ 
+ngOnInit(): void {
+  this.updateCurrentPage();
+}
+
+updateCurrentPage(): void {
+  const path = this.router.url.split('/')[1];
+  this.thisPage = (path || 'home') as typeof this.thisPage;
+}
 }
